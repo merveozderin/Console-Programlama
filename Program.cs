585add44c4f5420fc91e2a90269275f18a1c1e91
Merve <61613791+merveozderin@users.noreply.github.com>
2021-08-14 21:51:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Console_Programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi giriniz: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi giriniz: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
